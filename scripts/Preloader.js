fbf355243e40a98d714dedbf9411453df99e5c50
Dilshodcodes <yuldashevdilshodcodes@gmail.com>
2025-01-22 09:14:28 +0500

    stop

---
+++ scripts/Preloader.js
@@ -0,0 +1,4 @@
+setTimeout(() => {
+    const preloadContainer = document.querySelector('.preload-container');
+    preloadContainer.classList.add('preload-hidden');
+  }, 3000); // 3000 ms = 3 soniya
